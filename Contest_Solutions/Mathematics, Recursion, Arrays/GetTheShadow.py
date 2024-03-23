6d44fa75699dfd292f6e389252590bb5c8138389
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2024-03-23 09:56:38 +0530

    Update GetTheShadow.py

--- Contest_Solutions/Mathematics, Recursion, Arrays/GetTheShadow.py
+++ Contest_Solutions/Mathematics, Recursion, Arrays/GetTheShadow.py
@@ -1,1 +1,16 @@
+class Solution:
+    def solve(self, N, A):
+        def repeated_numbers(A):
+            act_a = 0
+            act_sum_sq = 0
+            
+            for num in A:
+                act_a += num
+                act_sum_sq += num * num
+            
+            exp_b = N * (N + 1) // 2
+            exp_sum_sq = N * (N + 1) * (2 * N + 1) // 6
 
+
+
+### Unsolved
