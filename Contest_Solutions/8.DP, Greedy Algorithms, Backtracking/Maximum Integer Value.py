5ab41e3254185df1be3c501a0c06553fed56ee1f
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2024-03-29 09:49:34 +0530

    Add files via upload

---
+++ Contest_Solutions/8.DP, Greedy Algorithms, Backtracking/Maximum Integer Value.py
@@ -0,0 +1,12 @@
+#User function Template for python3
+
+
+
+def maxValue(s):
+    #Your code here
+    a = int(s[0])
+    for i in range(1,len(s),1):
+        b = int (s[i])
+        a = max(a+b, a*b)
+    return a
+        
