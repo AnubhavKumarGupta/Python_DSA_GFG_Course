6244aaa7efe4b43558c85250986ae54fb44abd00
Anubhav Kumar Gupta <122034618+AnubhavKumarGupta@users.noreply.github.com>
2025-01-20 00:50:34 +0530

    Delete 2.Bitmagic, Matrix, Searching directory

--- 2.Bitmagic, Matrix, Searching/ToggleTheMiddle.py
+++
@@ -1,9 +0,0 @@
-import math
-def toggleTheMiddle(n):
-    numberOfBits=1+math.floor(math.log2(n))
-    if numberOfBits%2!=0:
-        n=n^(1<<(numberOfBits//2))
-    else:
-        n=n^(1<<(numberOfBits//2))
-        n=n^(1<<((numberOfBits//2)-1))
-    return n
